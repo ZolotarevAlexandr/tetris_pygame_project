619d8f7552a9731c2f3a2f337d320c05419bc6ef
ZolotarevAlexandr <Zolotarev10032006@mail.ru>
2023-01-21 14:17:00 +0300

    Basic game done

--- tetris.py
+++ tetris.py
@@ -24,14 +24,26 @@ blocks = [
      [7, 7]]
 ]
 
+colors = (
+    (0, 0, 0),
+    (0, 119, 211), # blue
+    (72, 93, 197), # dark blue
+    (120, 37, 111), # purple
+    (234, 20, 28), # red
+    (254, 251, 52), # yellow
+    (83, 218, 63), # green
+    (255, 200, 46) # orange
+)
+
 cell_size = 18
 cols = 10
 rows = 22
 FPS = 30
+drop_event = pygame.USEREVENT + 1
 
 
 def create_board():
-    board = [0 for x in range(cols)
+    board = [[0 for x in range(cols)]
              for y in range(rows)]
     return board
 
@@ -46,10 +58,30 @@ def rotate_clockwise(block):
 def destroy_filed_lines(board):
     new_board = board.copy()
     for line in board:
-        if line.count('0') == 0:
+        if line.count(0) == 0:
             del new_board[new_board.index(line)]
-            new_board.insert(0, ['0'] * 10)
+            new_board.insert(0, [0] * cols)
     return new_board
+
+
+def join_block(board, block_list, coords):
+    block_x, block_y = coords
+    for y, row in enumerate(block_list):
+        for x, val in enumerate(row):
+            board[y + block_y - 1][x + block_x] += val
+    return board
+
+
+def check_collision(board, shape, coords):
+    block_x, block_y = coords
+    for y, row in enumerate(shape):
+        for x, cell in enumerate(row):
+            try:
+                if cell and board[y + block_y][x + block_x]:
+                    return True
+            except IndexError:
+                return True
+    return False
 
 
 def quit_game():
@@ -70,8 +102,9 @@ class Tetris:
         self.level = 1
         self.score = 0
         self.lines = 0
-
         self.game_over = False
+
+        pygame.time.set_timer(drop_event, 200)
 
     def new_block(self):
         self.block = self.next_block[:]
@@ -79,7 +112,7 @@ class Tetris:
         self.block_x = int(cols / 2 - len(self.block[0]) / 2)
         self.block_y = 0
 
-        if self.check_collision():
+        if check_collision(self.board, self.block, (self.block_x, self.block_y)):
             self.game_over = True
 
     def move_block(self, movement):
@@ -90,12 +123,12 @@ class Tetris:
             if new_x > cols - len(self.block[0]):
                 new_x = cols - len(self.block[0])
 
-            if not self.check_collision():
+            if not check_collision(self.board, self.block, (new_x, self.block_y)):
                 self.block_x = new_x
 
     def rotate(self):
         rotated_block = rotate_clockwise(self.block)
-        if not self.check_collision():
+        if not check_collision(self.board, self.block, (self.block_x, self.block_y)):
             self.block = rotated_block
 
     def new_game(self):
@@ -109,11 +142,54 @@ class Tetris:
 
     def drop(self):
         self.block_y += 1
-        if self.check_collision():
+        if check_collision(self.board, self.block, (self.block_x, self.block_y)):
+            self.board = join_block(self.board, self.block, (self.block_x, self.block_y))
             self.new_block()
             self.board = destroy_filed_lines(self.board)
             return True
         return False
 
-    def check_collision(self):
-        pass
+    def render(self, board, coords):
+        block_x, block_y = coords
+        for y, row in enumerate(board):
+            for x, val in enumerate(row):
+                if val:
+                    pygame.draw.rect(
+                        self.screen,
+                        colors[val],
+                        pygame.Rect((block_x + x) * cell_size, (block_y + y) * cell_size,
+                                    cell_size, cell_size), 0)
+
+    def run(self):
+        clock = pygame.time.Clock()
+        while True:
+            self.screen.fill((0, 0, 0))
+            pygame.draw.line(self.screen,
+                             (255, 255, 255),
+                             (self.rlim + 1, 0),
+                             (self.rlim + 1, self.height - 1))
+
+            self.render(self.board, (0, 0))
+            self.render(self.block, (self.block_x, self.block_y))
+            self.render(self.next_block, (cols + 1, 2))
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT or event.type == pygame.K_ESCAPE:
+                    quit_game()
+                elif event.type == drop_event:
+                    self.drop()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.rotate()
+                    if event.key == pygame.K_LEFT:
+                        self.move_block(-1)
+                    if event.key == pygame.K_RIGHT:
+                        self.move_block(+1)
+
+            clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    Game = Tetris()
+    Game.run()
